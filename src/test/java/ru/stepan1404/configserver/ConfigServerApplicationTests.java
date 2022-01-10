c35deb5986f65b99f0f822a825437ffe4732519e
stepan14041999 <stepan.14041999@gmail.com>
2022-01-10 19:52:17 +0400

    Init commit

---
+++ src/test/java/ru/stepan1404/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stepan1404.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
